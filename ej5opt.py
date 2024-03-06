52a1e426334a6a9a868581c157ffe342fe893f33
donBravo1011 <130547772+donBravo1011@users.noreply.github.com>
2024-03-06 21:17:08 +0100

    Version 2.0
    Prueba Selenium

--- ej5opt.py
+++ ej5opt.py
@@ -1,5 +1,7 @@
+from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
+import time
 import json
 
 
@@ -7,7 +9,7 @@ class ScrapeStrategy():
     def scrape(self, url):
         pass
 
-
+"""
 class BeautifulSoupStrategy(ScrapeStrategy):
     def scrape(self, url, stock_symbol):
         response = requests.get(url)
@@ -39,10 +41,41 @@ class BeautifulSoupStrategy(ScrapeStrategy):
         else:
             return f'Failed to retrieve the webpage, status code: {response.status_code}'
 
-
+"""
 class SeleniumStrategy(ScrapeStrategy):
-    def scrape(self, url):
-        pass
+    def scrape(self, url, stock_symbol):
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        driver = webdriver.Chrome(options=options)
+        driver.get(url)
+        time.sleep(5)  # Espera para asegurarse de que la página esté completamente cargada
+        
+        # Encuentra los elementos utilizando Selenium
+        open_value_elem = driver.find_element_by_xpath("//td[@data-test='OPEN-value']")
+        close_value_elem = driver.find_element_by_xpath("//td[@data-test='PREV_CLOSE-value']")
+        volume_elem = driver.find_element_by_xpath("//td[@data-test='TD_VOLUME-value']")
+        market_cap_elem = driver.find_element_by_xpath("//td[@data-test='MARKET_CAP-value']")
+        
+        # Extrae el texto de los elementos
+        open_value = open_value_elem.text.strip() if open_value_elem else 'Open Value not found'
+        close_value = close_value_elem.text.strip() if close_value_elem else 'Close Value not found'
+        volume = volume_elem.text.strip() if volume_elem else 'Volume Value not found'
+        market_cap = market_cap_elem.text.strip() if market_cap_elem else 'Market Cap Value not found'
+        
+        # Crea un diccionario con los datos y el símbolo de la acción
+        data = {
+            'stock_symbol': stock_symbol,
+            'open': open_value,
+            'close': close_value,
+            'volume': volume,
+            'market_cap': market_cap
+        }
+        
+        # Cierra el navegador Selenium
+        driver.quit()
+        
+        return data
+
 
 
 class Context:
@@ -56,29 +89,37 @@ class Context:
         return self._strategy.scrape(url, stock_symbol)
 
 
-class introducir_beauti:
-    context = Context(BeautifulSoupStrategy())
-    values = context.scrape(url, stock_symbol)
-    print('Values:', values)
 
-    datos_requeridos1 = "datos_beautifulSoap.json"
+
+
+
     
-    with open(datos_requeridos1, "w") as archivo:
-        json.dump(values, archivo)
 
-
-class introducir_beauti:
-    datos_requeridos2 = "datos_seleniumStrategy.json"
-    #with open(datos_requeridos2, "w") as archivo:
-    #    json.dump(values, archivo)
 
 def main():
     stock_symbol = input("Enter the stock symbol (e.g., TSLA for Tesla): ")
     url = f'https://finance.yahoo.com/quote/{stock_symbol}'
     
+    """
+    #------------------------------------------------------------------Beautiful-----------------------------------------------------------------------
+    context = Context(BeautifulSoupStrategy())
+    values = context.scrape(url, stock_symbol)
+    print('Values BeautifulSoupStrategy:', values)
+
+    datos_requeridos1 = "datos_beautifulSoap.json"
     
-introducir_beauti
+    with open(datos_requeridos1, "w") as archivo:
+        json.dump(values, archivo)
+    """
+    #------------------------------Selenium-----------------------------------------------------------------------------------------------------------------
+    context = Context(SeleniumStrategy())
+    values = context.scrape(url,stock_symbol)
+    print('Values SeleniumStrategy:', values)
+
+    datos_requeridos2 = "datos_seleniumStrategy.json"
     
+    with open(datos_requeridos2, "w") as archivo:
+        json.dump(values, archivo)
 
 
 if __name__ == "__main__":
